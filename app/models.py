8d094ba867060caeb8fb2f0215c3ce910c5e42da
trolchiha <trotsko.olena@gmail.com>
2024-02-17 20:51:52 +0200

    created vote model and router

--- app/models.py
+++ app/models.py
@@ -19,3 +19,14 @@ class User(Base):
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
+class Vote(Base):
+    __tablename__ = "votes"
+    post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"), 
+                     primary_key=True, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), 
+                     primary_key=True, nullable=False)
+
+    post = relationship("Post")
+    user = relationship("User")
+    
--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from pydantic import BaseModel, ConfigDict, EmailStr
+from pydantic import BaseModel, ConfigDict, EmailStr, conint
 
 class UserBase(BaseModel):
     email: EmailStr
@@ -46,3 +46,7 @@ class TokenData(BaseModel):
 
     id: Optional[str] = None
     
+
+class Vote(BaseModel):
+    post_id: int
+    dir: conint(le=1) # type: ignore
