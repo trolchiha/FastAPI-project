0458a0e7205e9d3c12829f27023132aa9b5d3bfb
trolchiha <trotsko.olena@gmail.com>
2024-02-17 18:48:18 +0200

    added query parameters to get-post

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 from sqlalchemy.orm import Session
 from fastapi import Depends, Response, status, HTTPException, APIRouter
 
@@ -11,8 +11,9 @@ router = APIRouter(
 )
 
 @router.get("/", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    posts = db.query(models.Post).all()
+def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user),
+              limit: int = 10, skip: int = 0, search: Optional[str] = ""):
+    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
     return posts
 
 
