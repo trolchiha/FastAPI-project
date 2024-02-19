28c56af294eb96e8715aa725542b872912e71ea1
trolchiha <trotsko.olena@gmail.com>
2024-02-19 20:52:25 +0200

    configured CORS

--- app/main.py
+++ app/main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
-from . import models
-from .database import engine
+from fastapi.middleware.cors import CORSMiddleware
+
 from .routers import post, user, auth, vote
 
 # models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(auth.router)
 app.include_router(user.router)
@@ -13,5 +24,5 @@ app.include_router(post.router)
 app.include_router(vote.router)
 
 @app.get("/")
-def read_root():
+def main_page():
     return "Hello"
